e91fa308cfcd829dcee67cb7cda4e1c859ba53f5
Luis Fernando Arevalo <luisferarevalou@gmail.com>
2024-10-21 09:53:27 -0500

    port por PORT

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
+
 app.use('/', router)
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
 
-app.set('port', process.env.port || 3000)
+app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
   console.log('Conectados')
